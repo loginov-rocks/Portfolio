4b30fbb1f23a7b373f09f5798018bf64f7635118
Danila Loginov <danila@loginov.rocks>
2018-11-08 05:57:38 +0300

    keep Redux state in local storage

--- src/lib/store/createStore.js
+++ src/lib/store/createStore.js
@@ -1,15 +1,28 @@
 /* @flow */
 
 import { applyMiddleware, compose, createStore } from 'redux';
+import persistState, { mergePersistedState } from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
 import thunk from 'redux-thunk';
 
-export default (reducer) => {
+export default (rootReducer) => {
+  const reducer = compose(
+    mergePersistedState(),
+  )(rootReducer);
+
+  const middleware = [thunk];
+
+  const storage = adapter(window.localStorage);
+
   const composeEnhancers = (typeof window === 'object'
   && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : compose);
 
-  const middleware = [thunk];
+  const enhancer = composeEnhancers(
+    applyMiddleware(...middleware),
+    persistState(storage, process.env.REACT_APP_REDUX_STORAGE_KEY),
+  );
 
-  return createStore(reducer, composeEnhancers(applyMiddleware(...middleware)));
+  return createStore(reducer, enhancer);
 };
