085c85674fa23066069bf64595f7078f38a55a37
Danila Loginov <danila@loginov.rocks>
2018-11-09 04:11:21 +0300

    remove unused initial assets and refactor general files

--- src/App.js
+++ src/App.js
@@ -1,43 +1,29 @@
-import React, { Component } from 'react';
+/* @flow */
+
+import * as React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
 
 import { fetchDiff } from './actions';
-import './App.css';
 import AccountsList from './components/AccountsList';
 import Authorize from './components/Authorize';
 import LogoutButton from './components/LogoutButton';
 import Portfolio from './components/Porfolio';
 import withAuth from './enhancers/withAuth';
 import withStockQuotesUpdater from './enhancers/withStockQuotesUpdater';
-import logo from './logo.svg';
 
-class App extends Component {
+class App extends React.Component {
   componentDidMount() {
     this.props.fetchDiff();
   }
 
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <AccountsList />
         <Portfolio />
         <LogoutButton />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      </React.Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
+/* @flow */
+
+import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import createStore from './lib/store/createStore';
 import reducer from './reducer';
+import * as serviceWorker from './serviceWorker';
 
+const container = document.getElementById('root');
 const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  container,
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
