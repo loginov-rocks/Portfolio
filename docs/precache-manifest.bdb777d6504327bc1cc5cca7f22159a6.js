e39c8ffba9a8bb58f8f68fa23ce936aff0dce492
Danila Loginov <danila@loginov.rocks>
2018-11-09 04:46:09 +0300

    build production version into docs dir to launch GitHub pages

---
+++ docs/precache-manifest.bdb777d6504327bc1cc5cca7f22159a6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c03e00e4860b50885df",
+    "url": "/static/js/main.6c03e00e.chunk.js"
+  },
+  {
+    "revision": "b22bf99f4e9661cccbcc",
+    "url": "/static/js/1.b22bf99f.chunk.js"
+  },
+  {
+    "revision": "6bbcf4ecafaaf37d552cdc8d87da1992",
+    "url": "/index.html"
+  }
+];
