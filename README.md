012cbcea37e5815a6453c6fd22fa7d32cefadbec
Danila Loginov <danila@loginov.rocks>
2019-07-01 01:19:26 +0300

    configure deployment and dev server

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Portfolio
+
+[![Build Status](https://travis-ci.com/loginov-rocks/Portfolio.svg?branch=master)](https://travis-ci.com/loginov-rocks/Portfolio)
+[![dependencies Status](https://david-dm.org/loginov-rocks/Portfolio/status.svg)](https://david-dm.org/loginov-rocks/Portfolio)
+[![devDependencies Status](https://david-dm.org/loginov-rocks/Portfolio/dev-status.svg)](https://david-dm.org/loginov-rocks/Portfolio?type=dev)
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const HtmlPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-const aliases = ['App', 'Portfolio', 'Shared', 'Stocks', 'User', 'ZenMoney'];
-
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    port: 3000,
+  },
   entry: './src/index.tsx',
   module: {
     rules: [
@@ -19,8 +25,26 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: 'public',
+        ignore: 'index.html',
+      },
+    ]),
+    new Dotenv({
+      safe: false,
+    }),
+    new HtmlPlugin({
+      inject: false,
+      template: 'public/index.html',
+    }),
+  ],
   resolve: {
-    alias: Object.assign({}, ...aliases.map(dir => ({ [dir]: path.resolve(__dirname, `src/${dir}`) }))),
     extensions: ['.js', '.ts', '.tsx'],
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src'),
+    ],
   },
 };
