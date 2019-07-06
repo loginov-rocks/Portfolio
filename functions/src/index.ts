aeff27a05136f6ec33603f90ed97ec0ea1e6f85a
Danila Loginov <danila@loginov.rocks>
2019-07-06 17:08:28 +0300

    fix CI errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,18 @@
 // Import the Dialogflow module from the Actions on Google client library.
-import {dialogflow} from 'actions-on-google';
+import { dialogflow } from 'actions-on-google';
 // Import the firebase-functions package for deployment.
 import * as functions from 'firebase-functions';
 
 // Instantiate the Dialogflow client.
-const app = dialogflow({debug: true});
+const app = dialogflow({ debug: true });
 
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
-app.intent('favorite color', (conv, {color}) => {
-    // @ts-ignore
-    const luckyNumber = color.length;
-    // Respond with the user's lucky number and end the conversation.
-    conv.close('Your lucky number is ' + luckyNumber);
+app.intent('favorite color', (conv, { color }) => {
+  // @ts-ignore
+  const luckyNumber = color.length;
+  // Respond with the user's lucky number and end the conversation.
+  conv.close(`Your lucky number is ${luckyNumber}`);
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
