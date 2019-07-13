6c2c2078ff5f41d9d15446a8091ecaf21058b0fe
Danila Loginov <danila@loginov.rocks>
2019-07-13 13:45:38 +0300

    integrate redux-persist

--- src/App/components/App/App.enhancer.ts
+++ src/App/components/App/App.enhancer.ts
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import { isEmpty, isLoaded } from 'react-redux-firebase';
 import { branch, compose, withProps } from 'recompose';
+import { Persistor } from 'redux-persist';
 
 import { AuthState } from 'Firebase/State';
 import withStockQuotesUpdater from 'Stocks/enhancers/withStockQuotesUpdater';
@@ -8,15 +9,19 @@ import State from 'State';
 
 import { Props } from './App';
 
+interface EnhancedProps {
+  persistor: Persistor;
+}
+
 interface StateProps {
   auth: AuthState;
 }
 
 const mapStateToProps = ({ firebase: { firebase: { auth } } }: State): StateProps => ({ auth });
 
-export default compose<Props & StateProps, {}>(
+export default compose<Props & StateProps, EnhancedProps>(
   connect(mapStateToProps),
-  withProps<Props, StateProps>(({ auth }) => ({
+  withProps<Partial<Props>, StateProps>(({ auth }) => ({
     isAuthenticated: !isEmpty(auth) && isLoaded(auth),
     progress: !isLoaded(auth),
   })),
--- src/App/components/App/App.tsx
+++ src/App/components/App/App.tsx
@@ -1,6 +1,8 @@
 import { createMuiTheme, CssBaseline } from '@material-ui/core';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import * as React from 'react';
+import { Persistor } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import themeOptions from 'Styles/themeOptions';
 
@@ -10,12 +12,13 @@ import Loading from '../../views/Loading';
 
 export interface Props {
   isAuthenticated: boolean;
+  persistor: Persistor;
   progress: boolean;
 }
 
 const theme = createMuiTheme(themeOptions);
 
-const App: React.FunctionComponent<Props> = ({ isAuthenticated, progress }: Props) => {
+const App: React.FunctionComponent<Props> = ({ isAuthenticated, persistor, progress }: Props) => {
   let Component = null;
 
   if (progress) {
@@ -29,7 +32,9 @@ const App: React.FunctionComponent<Props> = ({ isAuthenticated, progress }: Prop
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      <Component />
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <Component />
+      </PersistGate>
     </MuiThemeProvider>
   );
 };
