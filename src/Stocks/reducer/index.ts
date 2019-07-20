21038f5d923643d14f521d3455735983063327cb
Danila Loginov <danila@loginov.rocks>
2019-07-20 15:52:30 +0300

    implement stocks data persistence and reconciliation

--- src/Stocks/reducer/index.ts
+++ src/Stocks/reducer/index.ts
@@ -1,9 +1,11 @@
 import { Action } from 'redux';
+import { REHYDRATE, RehydrateAction } from 'redux-persist';
 import { isResourceAction, repositoryReducer } from 'redux-repository/lib/reducer';
-import { createInitialState } from 'redux-repository/lib/repository';
+import { createInitialState, mergeRepositories } from 'redux-repository/lib/repository';
 import { Action as ReduxRepositoryAction } from 'redux-repository/lib/types';
 
 import * as C from 'Constants';
+import RootState from 'State';
 
 import Quote from '../lib/IEX/Quote';
 import State from '../State';
@@ -13,7 +15,22 @@ const initialState: State = {
   quotes: createInitialState(),
 };
 
-export default (state = initialState, action: Action) => {
+export default (state = initialState, action: Action): State => {
+  // Custom state reconciliation.
+  if (action.type === REHYDRATE) {
+    const { payload } = action as RehydrateAction<RootState>;
+
+    if (!payload) {
+      return state;
+    }
+
+    return {
+      ...state,
+      logos: mergeRepositories(state.logos, payload.stocks.logos),
+      quotes: mergeRepositories(state.quotes, payload.stocks.quotes),
+    };
+  }
+
   if (isResourceAction(C.STOCKS_LOGOS_RESOURCE_NAME, action as ReduxRepositoryAction<string, string>)) {
     return {
       ...state,
