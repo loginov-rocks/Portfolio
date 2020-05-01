ce25c460b1f792bd193689fc4629f4173447b19f
Danila Loginov <danila@loginov.rocks>
2020-05-01 20:05:16 +0300

    Functions: Update dependencies

--- functions/src/handlers/vibrantPalette.ts
+++ functions/src/handlers/vibrantPalette.ts
@@ -2,4 +2,4 @@ import { Request } from 'firebase-functions/lib/providers/https';
 import Vibrant from 'node-vibrant';
 import { Palette } from 'node-vibrant/lib/color';
 
-export default (request: Request): Promise<Palette> => Vibrant.from(request.query.img).getPalette();
+export default (request: Request): Promise<Palette> => Vibrant.from(request.query.img as string).getPalette();
